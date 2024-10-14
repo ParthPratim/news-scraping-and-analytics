4cdee317e72d3600462bdbda9166a776730febd8
Parth Pratim Chatterjee <parth27official@gmail.com>
2024-10-15 02:43:01 +0530

    Added 1 schema outline and some helper stats api classes

--- core/stats/api.py
+++ core/stats/api.py
@@ -1,2 +1,19 @@
+class APIResponse:
 
+    def __init__(self):
+        pass
+
+
+class KeywordTrend(APIResponse):
+    def __init__(self, **kwargs):
+        pass
+    
+    def Keyword(self):
+        pass
+
+    def DateList(self):
+        pass
+
+    def ArticleCountList(self):
+        pass
 
