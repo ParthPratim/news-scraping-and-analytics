4108678e9b4aedac3943c48ef2007badae82a556
Parth Pratim Chatterjee <parth27official@gmail.com>
2024-10-15 16:53:43 +0530

    Added bs4 object to recurive parser constructor

--- core/utils/parser.py
+++ core/utils/parser.py
@@ -1,3 +1,6 @@
+import json
+import os
+
 """
 Article Scraping Format
 -------------------
@@ -12,3 +15,30 @@ scraped for each article
                                 scraped
     7. Categories           : Categories and Subcategories this sample is from
 """
+
+class TimesNowScrapper :
+    def __init__(self, start_time : int, end_time : int):
+        self.url_prefix = "https://timesofindia.indiatimes.com/archivelist/starttime-"
+        self.url_suffx = ".cms"
+        pass
+
+
+class RecursiveParser:
+
+    def __init__(self, config, bs4Obj = None , schema_file = None):
+        self.config = config
+        self.schema_file = schema_file
+        self.bs4Obj = bs4Obj
+        self.schema = self.config.DEFAULT_SCHEMA
+
+    def GetSchemaFile(self):
+        return os.path.join(self.config.SCHEMA_DIRECTORY, self.schema_file)
+
+    def LoadSchema(self):
+        with open(self.GetSchemaFile(), 'r') as jschema:
+            return json.load(jschema)
+
+    def DoParse(self):
+        self.schema = self.LoadSchema()
+        # Write logic for parsing the schema and use bs4 for parsing the website
+        # using the schema
